a4171ab9acabe5a651594fffe42e7d49143da4af
Mehdi-ehsani <metti.work@gmail.com>
2025-03-15 05:55:17 +0330

    feat: implement user fetching with search and pagination on the home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,46 @@
-export default function Home() {
-  return (
-    <h1>
-      hi metti
-    </h1>
-  );
+import UserTable from "@/components/UserTable";
+import UserModal from "@/components/UserModal";
+
+export default async function Home({ searchParams }) {
+	try {
+		const page = searchParams?.page ? parseInt(searchParams.page, 10) : 1;
+		const limit = 5;
+		const searchQuery = searchParams?.search || "";
+
+		const res = await fetch(
+			`https://jsonplaceholder.typicode.com/users?q=${searchQuery}&_page=${page}&_limit=${limit}`
+		);
+		const data = await res.json();
+		if (!res.ok) {
+			throw new Error("Failed to fetch users");
+		}
+
+		const totalUsersRes = await fetch(
+			`https://jsonplaceholder.typicode.com/users?q=${searchQuery}`
+		);
+		const totalUsers = await totalUsersRes.json();
+		const totalPages = Math.ceil(totalUsers.length / limit);
+		if (!totalUsersRes.ok) {
+			throw new Error("Failed to fetch users");
+		}
+		return (
+			<div>
+				<UserTable
+					usersData={data}
+					totalPages={totalPages}
+					currentPage={page}
+				/>
+				<UserModal />
+			</div>
+		);
+	} catch (error) {
+		return (
+			<div className="flex flex-col items-center justify-center h-screen">
+				<h1 className="text-red-500 text-2xl">
+					خطا در دریافت اطلاعات کاربران!
+				</h1>
+				<p className="text-gray-400">لطفاً بعداً دوباره تلاش کنید.</p>
+			</div>
+		);
+	}
 }
