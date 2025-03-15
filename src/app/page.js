84abdbb63eb4c3216078e26afc8a30f374e8643d
Metti <metti.work@gmail.com>
2025-03-15 15:28:19 +0330

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -37,7 +37,7 @@ export default async function Home({ searchParams }) {
 		return (
 			<div className="flex flex-col items-center justify-center h-screen">
 				<h1 className="text-red-500 text-2xl">
-        "Failed to fetch users"
+        Failed to fetch users
 				</h1>
 				<p className="text-gray-400">Please Try Again Later</p>
 			</div>
